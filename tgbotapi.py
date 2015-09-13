65b922d16555c87ad073b77b59ed0218e9991ff6
Jose Luis <jcjoseluiscd@gmail.com>
2015-09-13 21:39:16 +0200

    Added tgbotapi.py

---
+++ tgbotapi.py
@@ -0,0 +1,27 @@
+import requests
+from functools import partial
+
+BOT_API_URL="https://api.telegram.org/bot"
+
+class TgException(Exception):
+	pass
+
+class TgBot:
+	def __init__(self, token):
+		self.token = token
+		
+	def call_method(self, method, files: dict=None, **data):
+		ret = requests.post(BOT_API_URL+self.token+"/"+method, data= data if data else None, files=files if files else None)
+		if ret.status_code==requests.codes.ok:
+			l = ret.json()
+			if l["ok"]:
+				return l["result"]
+			elif "description" in l:
+				raise TgException(l["description"])
+			else:
+				raise TgException("Error. Respuesta recibida: "+str(l))
+		else:
+			ret.raise_for_status()
+
+	def __getattr__(self, name):
+		return partial(self.call_method, name)
