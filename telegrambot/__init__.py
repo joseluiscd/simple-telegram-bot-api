ee85074dcf881fb2a65f36de45cbfdb49e9b6fa3
José Luis C.D <jlcd0003@red.ujaen.es>
2016-07-24 18:04:16 +0200

    Middleware, fixed requirements

--- telegrambot/__init__.py
+++ telegrambot/__init__.py
@@ -1,15 +1,21 @@
 from . import tgbotapi
 import re
+import time
 
 class TgCommandBot(tgbotapi.TgApiConnection):
 	def __init__(self, token, webhook=True):
 		super().__init__(token)
-		self.me = self.getMe()
+		try:
+			self.me = self.getMe()
+		except:
+			time.sleep(5)
+			
 		self.cmdregex = re.compile("^/(?P<command>[a-zA-Z0-9]+)(|\@(?P<bot>[a-zA-Z0-9]+))($| )")
 
 	handlers = dict()
 	hooks_user = dict()
 	hooks_group = dict()
+	callbacks = dict()
 	middleware = list()
 
 	def runMiddleware(self, message):
@@ -24,19 +30,24 @@ class TgCommandBot(tgbotapi.TgApiConnection):
 
 		return message
 
-	def middlewareHandler(self, func):
-		print("New middleware")
-		self.middleware.append(func)
-
 	def processUpdate(self, update):
 		if "message" in update:
 			return self.processMessage(update["message"])
+		elif "callback_query" in update:
+			return self.processCallbackQuery(update["callback_query"])
+
+	def processCallbackQuery(self, callback_query):
+		for regex in self.callbacks:
+			match = regex.match(callback_query["data"])
+			if match:
+				self.callbacks[regex](self, callback_query, match)
+
 
 	def processMessage(self, message):
 		message = self.runMiddleware(message)
 		if message is None:
 			return
-			
+
 		#Hooks
 		if message["chat"]["type"]=="private" and message["chat"]["id"] in self.hooks_user: # Hook para el usuario
 			if "text" in message and message["text"]=="/cancel":
@@ -85,6 +96,17 @@ class TgCommandBot(tgbotapi.TgApiConnection):
 			self.handlers[fn_condition] = func
 
 		return decorator
+
+	def callbackQueryHandler(self, regex):
+		def decorator(fn):
+			self.callbacks[regex] = fn
+			return fn
+		return decorator
+
+	def middlewareHandler(self, func):
+		print("New middleware")
+		self.middleware.append(func)
+		return func
 
 	def messageHandler(self, msgtype="text", group=True, private=True, sudo=False, command=None, custom_fn=None):
 		def fn_condition(bot, message):
